0e875f4b0b3069c8ce6abded6554e38ac177a402
shariard58 <shariard58@gmail.com>
2024-10-24 04:10:51 +0600

    initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import MovieCard from "./components/home/MovieCard";
 export default function Home() {
   return (
     <div>
-      <p>Movie Hunt Project</p>
+      <div>
+        <h2 className="sm:text-[18px] md:text-[22px] lg:text-[18px] mb:2 md:mb-3 font-extrabold">
+          All Movies
+        </h2>
+        <MovieCard />
+      </div>
     </div>
   );
 }
