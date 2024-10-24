db0c46bbec91f6218e0ccd01016ea2403d57ffc8
shariard58 <shariard58@gmail.com>
2024-10-24 08:26:33 +0600

    demo watch list page

---
+++ app/watchlist/page.jsx
@@ -0,0 +1,8 @@
+export default function Watchlist() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">My Watchlist</h1>
+      <p className="mt-4">This is where your watchlist will be displayed.</p>
+    </div>
+  );
+}
