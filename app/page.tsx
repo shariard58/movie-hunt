b97ea2fa4a1454f906f36a3fa097206075dcc924
shariard58 <shariard58@gmail.com>
2024-10-24 11:59:41 +0600

    page.jsx modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import MovieCard from "./components/home/MovieCard";
+import AllMovies from "./components/home/AllMovies";
 
 export default function Home() {
-  const movies = Array.from({ length: 24 }, (_, index) => ({
-    id: index + 1,
-    title: `Movie ${index + 1}`,
-    genre: "Action",
-    imageUrl: "/assets/joker.jpg",
-  }));
-
   return (
-    <div>
-      <h2 className="sm:text-[18px] md:text-[22px] lg:text-[18px] mb:2 md:mb-3 font-extrabold">
-        All Movies
-      </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} />
-        ))}
-      </div>
+    <div className="border border-black px-4">
+      <AllMovies />
     </div>
   );
 }
