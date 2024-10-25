b2437dbd69022c8608b238d4a49e134156e537c0
shariard58 <shariard58@gmail.com>
2024-10-25 15:23:02 +0600

    data validated by scheam while api fetching

---
+++ lib/movieDetailsApi.ts
@@ -0,0 +1,40 @@
+// Fetch the movie details from TMDB API
+import { z } from "zod";
+import { CastDetailsSchema, MovieDetailsSchema } from "./schemas";
+
+// Creating  TypeScript types from the Zod schemas
+type MovieDetails = z.infer<typeof MovieDetailsSchema>;
+type CastDetails = z.infer<typeof CastDetailsSchema>;
+
+// Fetch movie details
+export const fetchMovieDetails = async (id: string): Promise<MovieDetails> => {
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch movie details");
+  }
+  const data = await response.json();
+  return MovieDetailsSchema.parse(data);
+};
+
+// Fetch cast information
+export const fetchMovieCast = async (id: string): Promise<CastDetails> => {
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch movie cast");
+  }
+
+  const data = await response.json();
+
+  if (!data || !data.cast) {
+    throw new Error("No data received for cast");
+  }
+
+  return data;
+};
