32e6210324108cd94fd79c077687c374359b08ed
shariard58 <shariard58@gmail.com>
2024-10-25 16:53:13 +0600

    data adde dynamically for related movie cared

--- app/components/movieDetails/RelatedMovieCard.tsx
+++ app/components/movieDetails/RelatedMovieCard.tsx
@@ -1,19 +1,31 @@
+import { RelatedMovie } from "@/types/relatedMovieTypes";
 import Image from "next/image";
 import Link from "next/link";
 
-const RelatedMovieCard = () => {
+type RelatedMovieCardProps = {
+  movie: RelatedMovie;
+};
+
+const RelatedMovieCard = ({ movie }: RelatedMovieCardProps) => {
+  const starCount = movie.vote_average ? Math.round(movie.vote_average / 2) : 0;
   return (
-    <figure className="shadow-md p-3 border border-gray-200 rounded-lg sm:max-w-[280px] h-[300px] flex flex-col transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer">
+    <figure className="relative shadow-lg p-3 border border-gray-200 rounded-lg sm:max-w-[280px] h-[350px] flex flex-col transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer overflow-hidden">
       <div
-        className="bg-cover bg-center h-[140px] rounded-lg overflow-hidden"
+        className="bg-cover bg-center h-[200px] rounded-lg"
         style={{
-          backgroundImage: "url('/assets/joker.jpg')",
+          backgroundImage: movie.backdrop_path
+            ? `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`
+            : "url('/assets/joker.jpg')",
         }}
       />
       <figcaption className="flex-grow pt-2 flex flex-col justify-between">
-        <div>
-          <h3 className="text-lg mb-1 font-semibold">Joker</h3>
-          <p className="text-gray-600 text-sm mb-2">Drama/Thriller</p>
+        <div className="flex-grow">
+          <h3 className="text-lg mb-1 font-semibold line-clamp-1">
+            {movie.title || "Untitled"}
+          </h3>
+          <p className="text-gray-600 text-sm mb-2 line-clamp-2">
+            {movie.overview || "No overview available."}
+          </p>
           <div className="flex items-center justify-start space-x-1 mb-2">
             {[...Array(5)].map((_, index) => (
               <Image
@@ -22,14 +34,17 @@ const RelatedMovieCard = () => {
                 width={14}
                 height={14}
                 alt="Star Icon"
+                className={`${
+                  index < starCount ? "text-yellow-400" : "text-gray-400"
+                }`}
               />
             ))}
           </div>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between space-x-2">
           <Link
             className="bg-[#FF8C00] rounded-lg py-1 px-3 text-white text-sm font-semibold"
-            href="/movies/1"
+            href={`/movies/${movie.id}`}
             scroll={false}
           >
             View Details
