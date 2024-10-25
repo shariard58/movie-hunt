e2b8c40ce44a091b47a99553ec449f17704e85b2
shariard58 <shariard58@gmail.com>
2024-10-25 11:24:03 +0600

    moving the search bar from the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
-import SearchInput from "./input/SearchInput";
 
 export default function Header() {
   return (
     <header className="flex flex-col md:flex-row justify-between items-start md:p-4 space-y-4 md:space-y-0">
-      <SearchInput />
-
       <div className="hidden md:flex space-x-6">
         <ul className="flex space-x-6">
           <li className="hover:text-orange-700 hover:font-bold transition-colors duration-200 cursor-pointer bg-gradient-to-r from-[#FF8C00] to-[#FFA500] bg-clip-text text-transparent">
