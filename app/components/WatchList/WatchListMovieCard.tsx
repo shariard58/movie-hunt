d0e418ef715a3cad28a1626e726e61113e7d8f34
shariard58 <shariard58@gmail.com>
2024-10-26 18:32:31 +0600

    delete fuction imporoved

--- app/components/WatchList/WatchListMovieCard.tsx
+++ app/components/WatchList/WatchListMovieCard.tsx
@@ -2,13 +2,41 @@
 import { SingleMovieType } from "@/types/movieTypes";
 import Image from "next/image";
 import Link from "next/link";
+import { FC } from "react";
+import { toast } from "react-toastify";
 
-export default function WatchListMovieCard({ movie }: SingleMovieType) {
+// Define the type for the refreshWatchlist function
+type WatchListMovieCardProps = {
+  movie: SingleMovieType;
+  refreshWatchlist: () => void; // Define refreshWatchlist as a function with no parameters returning void
+};
+
+const WatchListMovieCard: FC<WatchListMovieCardProps> = ({
+  movie,
+  refreshWatchlist,
+}) => {
   const starRating = Math.round((movie.vote_average ?? 6) / 2);
   const movieId = movie?.id ? movie.id.toString() : "";
 
-  const removeFromWatchlist = (id: string) => {
-    console.log(id);
+  const removeFromWatchlist = async (id: string | number) => {
+    try {
+      const response = await fetch("/api/watchList", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: parseInt(id as string, 10) }),
+      });
+
+      if (response.ok) {
+        refreshWatchlist();
+        toast.success("Removed from Watchlist!");
+      } else {
+        console.error("Failed to remove movie from watchlist");
+      }
+    } catch (error) {
+      console.error("Error removing movie from watchlist:", error);
+    }
   };
   return (
     <div className="bg-white shadow-md p-4 rounded-lg flex flex-col transition-transform duration-300 ease-in-out hover:scale-105">
@@ -24,7 +52,9 @@ export default function WatchListMovieCard({ movie }: SingleMovieType) {
 
       {/* Movie Details */}
       <h3 className="text-lg sm:text-xl font-semibold mb-2">{movie.title}</h3>
-      <p className="text-gray-600 text-sm mb-2">{movie.genre}</p>
+      <p className="text-gray-600 text-sm mb-2">
+        {movie.genres?.map((genre) => genre.name).join(", ")}
+      </p>
 
       {/* Star Rating */}
       <div className="flex items-center justify-start space-x-1 mb-4">
@@ -49,11 +79,13 @@ export default function WatchListMovieCard({ movie }: SingleMovieType) {
         </Link>
         <button
           className="bg-gray-200 rounded-lg py-2 px-3 sm:px-4 text-sm font-semibold text-black"
-          onClick={removeFromWatchlist(movieId)}
+          onClick={() => removeFromWatchlist(movieId)} // Ensure function reference
         >
           Remove
         </button>
       </div>
     </div>
   );
-}
+};
+
+export default WatchListMovieCard;
