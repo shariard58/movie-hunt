3fc0457c7e2dd9875fa5f68d39f43ac27bcfee65
shariard58 <shariard58@gmail.com>
2024-10-26 16:26:15 +0600

    new type created for single movie item

--- types/movieTypes.ts
+++ types/movieTypes.ts
@@ -14,3 +14,34 @@ export type MovieType = {
   vote_average?: number;
   vote_count?: number;
 };
+
+export type SingleMovieType = MovieType & {
+  belongs_to_collection?: {
+    id: number;
+    name: string;
+    poster_path: string | null;
+    backdrop_path: string | null;
+  } | null;
+  budget?: number;
+  homepage?: string;
+  imdb_id?: string;
+  genres?: {
+    id: number;
+    name: string;
+  }[];
+  production_companies?: {
+    id: number;
+    logo_path?: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status?: string;
+  tagline?: string;
+};
